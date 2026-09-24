d3c596fbbabc09031abf478d9a7b0f1891624d3c
Jeremy Chesnel <jeremy.chesnel@meteo.fr>
2026-09-24 13:31:55 +0000

    Change open mpi version in ial compilation with rocm

--- scripts/ial-rocm-cpu1.sh
+++ scripts/ial-rocm-cpu1.sh
@@ -3,14 +3,14 @@
 set -x
 set -e
 
-tt=CPU_O1_ROCM2321_OPENMPI5.0.7
+tt=CPU_O1_ROCM2321_OPENMPI5.0.10
 
 \rm -f build.$tt/bin/MASTERODB
 
 ../IAL/bundle/ial-bundle \
   build  \
   --build-dir build.$tt \
-  -j 64 \
+  -j 16 \
   --arch ../IAL/bundle/arch/fxtran/ \
   --verbose \
   --build-type FXTRAN_${tt} \
